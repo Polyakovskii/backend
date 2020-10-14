6e100b7d6ad1c04eb18f627200591089592a85a0
Polyakovskii <a.polyakovskii@gmail.com>
2020-10-14 10:41:51 +0300

    Removed makemigrations from entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -12,7 +12,6 @@ fi
 
 # Make migrations and migrate the database.
 echo "Making migrations and migrating the database. "
-python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 python manage.py runserver 0.0.0.0:8000
 
